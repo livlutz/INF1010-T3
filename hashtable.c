39bc8b741550eddcda60022774b7d91f262a3bf8
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2023-04-29 18:56:28 -0300

    Comentários das funções

--- hashtable.c
+++ hashtable.c
@@ -7,20 +7,26 @@
 #include <string.h>
 #include "hashtable.h"
 
-#define TAMINICIAL 1031 //primo após 1024 
+#define TAMINICIAL 1031 //Primo após 1024 
 
-//encadeamento interior
+//Usamos encadeamento interior
+
+//Struct do array de informações da placa
 
 typedef struct {
-	int chave;
-	char dados[8];
-	int prox;
+	int chave; // Chave da placa no array
+	char dados[8]; //Placa
+	int prox; //Número da próxima posição livre no array
 } ttabpos;
 
+//Struct da hastable 
+
 struct smapa {
-	int tam;
-	ttabpos* tabpos;
+	int tam; //Tamanho da hashtable
+	ttabpos* tabpos; //Ponteiro para o array de placas
 };
+
+//Função de hash por método da multiplicação ("meio do quadrado") 
 
 int hash(int key) {
 	key *= key;
@@ -30,7 +36,12 @@ int hash(int key) {
 	return key % TAMINICIAL;
 }
 
+//Função de criar a hashtable
+
 Mapa* cria(void) {
+	
+	//Cria hashtable
+	
 	int i;
 	Mapa* m = (Mapa*)malloc(sizeof(Mapa));
 
@@ -38,6 +49,8 @@ Mapa* cria(void) {
 		printf("erro na alocação! \n"); 
 		exit(1); 
 	}
+	
+	//Cria array de placas
 
 	m->tabpos = (ttabpos*)malloc(TAMINICIAL * sizeof(ttabpos));
 	if (m->tabpos == NULL) {
@@ -46,32 +59,54 @@ Mapa* cria(void) {
 	}
 
 	m->tam = TAMINICIAL;
+	
+	//inicializa as chaves e os próximos com -1
 
 	for (i = 0; i < TAMINICIAL; i++) {
 		m->tabpos[i].chave = -1;
 		m->tabpos[i].prox = -1;
 	}
+	
+	//retorna a hashtable
 
 	return m;
 }
 
 int insere(Mapa* m, int c, char* d) {
+	
+	//calcula posição da chave no array usando hash()
 
 	int pos = hash(c);
+	
+	//contador de colisões e posições
+	
 	int col = 0, i = 1;
+	
+	//se houver espaço para alocar os dados no índice calculado, insere normalmente retornando 0 colisões
 
 	if (m->tabpos[pos].chave == -1) {
 		m->tabpos[pos].chave = c;
 		strcpy(m->tabpos[pos].dados, d);
+		m->tabpos[pos] = m->tabpos[pos];
 		return col;
 	}
+	
+	//senão buscamos a próxima posição livre no índice e adicionamos as informações dos parâmetros lá
+	//adicionamos 1 na colisão para cada posição nao livre no array
+	
 	for (; m->tabpos[pos + i].chave != -1; i++) {
 		col++;
+		
+		//se chegarmos ao fim dos índices da hashtable, exibimos uma mensagem de erro e retornamos o número de colisões
+		
 		if ((pos + i) == 1030) {
 			printf("Erro ao inserir.\n");
 			return col;
 		}
 	}
+	
+	//Fazemos a inserção das informações dos parâmetros na posição livre encontrada e retornamos o número de colisões
+	
 	m->tabpos[pos + i].chave = c;
 	strcpy(m->tabpos[pos + i].dados, d);
 	m->tabpos[pos + i].prox = p.prox;
@@ -80,25 +115,38 @@ int insere(Mapa* m, int c, char* d) {
 }
 
 int busca(Mapa*m,int c){
+	
+	//Pegamos o índice da chave procurada no array e inicializamos o contador de posições
 
 	int pos = hash(c);
 	int i = 1;
+	
+	//Se a primeira posição do índice for a buscada, retornamos o valor da posição
 
 	if (m->tabpos[pos].chave == c) {
 		return pos;
 	}
+	
+	//senão buscamos dentro da posição até encontrar a chave procurada
 
 	else {
 		for (; m->tabpos[pos+i].prox != -1; i++) {
+			
+			//se for encontrada, retornamos sua posição
+			
 			if (m->tabpos[pos + i].chave == c) {
 				return pos + i;
 			}
 		}
 	}
+	
+	//caso chegue na última posição (-1) retornamos pos + i
 
 	if (m->tabpos[pos + i].chave == c) {
 		return pos + i;
 	}
+	
+	//se não encontrou a chave, retornamos -1
 
 	return -1;
 	
