8f7b2ee091219743ecfd63bf87c08ad76357eb12
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2023-04-29 18:58:57 -0300

    Comentários dos protótipos das funções

--- hashtable.h
+++ hashtable.h
@@ -1,9 +1,26 @@
 /* Lívia Lutz dos Santos - 2211055 */
 /* Luiza Marcondes Paes Leme - 2210275 */
 
+//Struct do array de placas
+
 typedef struct ttabpos;
+
+//Struct da hash table
+
 typedef struct smapa Mapa;
+
+//Função que cria a hashtable
+
 Mapa* cria(void);
+
+//Função que insere infromações das placas na hashtable
+
 Mapa* insere(Mapa* m, int chave, char* dados);
+
+//Função de hash que gera o índice de cada chave das placas no array
+
 int hash(int key);
+
+//Função que busca uma chave de placa na hash table
+
 int busca(Mapa* m, int c);
